1d8a35787f4c0de8179272a4179c583841fcce0e
generalkrishna <everyfor0324@gmail.com>
2019-06-05 11:27:32 +0900

    0605修正。

--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -15,27 +15,27 @@ class MainModal extends React.Component {
             </div>
             <div class="popup-content">
               <div class="popup-title">
-                <h4 class="title txt-light">Almost here!</h4>
-                <p class="subtitle txt-light-transparent">Subscribe to our newsletter and we will send you a notification about the launch of our brand new site.</p>
+                <h4 class="title txt-light">ここです!</h4>
+                <p class="subtitle txt-light-transparent">ニュースレターを購読すると,弊社の新しいサイトの発売についてお知らせいたします。</p>
               </div>
               <div class="content-block">
                 <form class="notify-form">
                   <div class="form-group form_row_name">
-                    <input class="form-control" type="text" placeholder="First Name*"/>
-                    <input class="form-control" type="text" placeholder="Last Name*"/>
+                    <input class="form-control" type="text" placeholder="姓"/>
+                    <input class="form-control" type="text" placeholder="名"/>
                   </div>
                   <div class="form-group ">
-                    <input class="form-control" type="text" placeholder="Email Adress*"/>
+                    <input class="form-control" type="text" placeholder="メールアドレス*"/>
                   </div>
                   <div class="form-group ">
-                    <input class="form-control" type="text" placeholder="Mobile*"/>
+                    <input class="form-control" type="text" placeholder="モバイル"/>
                   </div>
                   <div class="form-group ">
-                    <input  type="file" id="form_file" class="dis_none" placeholder="First Name*"/>
-                    <label id="file_control" class="form-control" for="inputGroup">Choose pictures*</label>
+                    <input  type="file" id="form_file" class="dis_none" />
+                    <label id="file_control" class="form-control" for="inputGroup">画像を選択する*</label>
                   </div>
                   <div class="form-group ">
-                    <button class="btn btn-l btn-light" type="submit"><span class="btn-caption">Send</span></button>
+                    <button class="btn btn-l btn-light" type="submit"><span class="btn-caption">送信</span></button>
                   </div>
                 </form>
               </div>
